6cdfb362ad3387b697978a7e645067810cdd7ac2
Cherepanov Andrey <cherepanov.andrey944@gmail.com>
2021-12-01 14:11:44 +0300

    add link constant

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,14 @@
 from pages.main_page import MainPage
+LINK_MAIN_PAGE = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
 
 def test_guest_can_go_to_login_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/'
+    link = LINK_MAIN_PAGE
     page = MainPage(browser=browser, url=link)
     page.open()
     page.go_to_login_page()
 
 def test_guest_should_see_login_link(browser):
-    link = 'http://selenium1py.pythonanywhere.com/'
+    link = LINK_MAIN_PAGE
     page = MainPage(browser=browser, url=link)
     page.open()
     page.should_be_login_link()
