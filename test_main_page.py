555e37e0e4136734ec3cec3ac86a4e7a9f612765
Cherepanov Andrey <cherepanov.andrey944@gmail.com>
2021-12-03 01:19:45 +0300

    add test guest_cant_see_product_in_basket_opened from main page

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,7 @@
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
+import pytest
 LINK_MAIN_PAGE = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser):
@@ -15,3 +17,13 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser=browser, url=link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser, request):
+    link = LINK_MAIN_PAGE
+    language = request.config.getoption('language')
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_basket_items()
+    basket_page.should_be_empty_basket_text(language)
