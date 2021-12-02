1cefc5d1eb399894f139b14cd273a6ea050d6981
Cherepanov Andrey <cherepanov.andrey944@gmail.com>
2021-12-03 02:05:34 +0300

    add test login page class

--- test_main_page.py
+++ test_main_page.py
@@ -4,19 +4,21 @@ from pages.basket_page import BasketPage
 import pytest
 LINK_MAIN_PAGE = "http://selenium1py.pythonanywhere.com/"
 
-def test_guest_can_go_to_login_page(browser):
-    link = LINK_MAIN_PAGE
-    page = MainPage(browser=browser, url=link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = LINK_MAIN_PAGE
+        page = MainPage(browser=browser, url=link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-def test_guest_should_see_login_link(browser):
-    link = LINK_MAIN_PAGE
-    page = MainPage(browser=browser, url=link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        link = LINK_MAIN_PAGE
+        page = MainPage(browser=browser, url=link)
+        page.open()
+        page.should_be_login_link()
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser, request):
     link = LINK_MAIN_PAGE
